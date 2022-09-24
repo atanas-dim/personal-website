0640ac26aa1b921c89d85e9336253bc5556c72b8
Atanas Dimitrov <hi.atanasdim@gmail.com>
2022-09-24 22:54:44 +0100

    Update Hero.tsx

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,7 +4,7 @@ import { PERFORATED_BG } from "../../styles/constants";
 
 const Hero: FC = () => {
   return (
-    <div className="w-full h-full relative flex justify-center items-center flex-col">
+    <div className="w-full h-full relative flex justify-center items-center flex-col z-10">
       <div className="absolute z-[-20] top-0 left-0 w-full h-full bg-radial-gradient-pink bg-no-repeat bg-center opacity-70" />
       <div
         className={`absolute z-[-10] top-0 left-0 w-full h-full ${PERFORATED_BG} bg-left-bottom`}
