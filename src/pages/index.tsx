04038be40631fd4571e0306fcb03a298d7e7b845
Atanas Dimitrov <hi.atanasdim@gmail.com>
2022-09-25 19:10:49 +0100

    Update html tags and change scroll container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,27 +7,30 @@ import IPhone14 from "../components/devices/IPhone14";
 
 const IndexPage = () => {
   return (
-    <main className="w-full h-full overflow-y-auto snap-mandatory snap-y">
+    <>
       <Hero />
       <Header />
-
-      <div className="min-h-[200vh] px-4 md:px-8 snap-start scroll-mt-8">
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingTop: 32,
-          }}
-        >
-          <h2 className="text-purple-500 text-4xl font-bold mb-8">Projects</h2>
-          <IPhone14 width={300} />
-          <IPhone14 width={300} />
-          <IPhone14 width={300} />
-        </div>
-      </div>
-    </main>
+      <main className="w-full h-full ">
+        <section className="min-h-[200vh] px-4 md:px-8 snap-start scroll-mt-8">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: 32,
+            }}
+          >
+            <h2 className="text-purple-500 text-4xl font-bold mb-8">
+              Projects
+            </h2>
+            <IPhone14 width={300} />
+            <IPhone14 width={300} />
+            <IPhone14 width={300} />
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
